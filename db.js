1462cb918f4a50d71315d46de2c8f236f14b997e
Darielly Santos <daryh_cristina@hotmail.com>
2021-10-14 12:05:44 -0300

    fead: segurança

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
 const Sequelize = require("sequelize");
-const sequilize = new Sequelize(
-  "tabela_teste_selecao",
-  "dariellysantos",
-  "123456",
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+const sequilize = new Sequelize("tabela_teste_selecao", "", "", {
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306,
+});
 
 module.exports = sequilize;
